078fba5324c8b76b35a2b6bc7186dbfc90f48390
Luke Bearl <luke@bearl.me>
2016-06-28 07:05:04 -0700

    Add project files.

---
+++ NuGetVersion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NuGetVersion.Startup))]
+namespace NuGetVersion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
